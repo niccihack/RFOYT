520911cb67f11f905f1d63afa515def18e5d7673
niccihack <niccihack@gmail.com>
2017-02-15 22:31:50 -0800

    Tag numbers subset and matching unique fish subset. Duplicate records need to be delt with still.

--- fishing data.R
+++ fishing data.R
@@ -10,6 +10,13 @@
 library(readr)
 Fishing_data <- read_csv("C:/Users/manic/Dropbox/iRock/CCFRP blood samples/Fishing data.txt")
 View(Fishing_data)
+#load CCFRP data
+library(readxl)
+Hack_Sample_Data <- read_excel("C:/Users/manic/Dropbox/Home computer/CalPoly/Rockfsh/Hack_Sample_Data.xlsx")
+View(Hack_Sample_Data)
+
+
+#####Formatting my data#####
 #adjust my data to match CCFRP
 #because cell is coded as site.cell without a deliminiter, I have to recreate this in my data
 ahackdat<-Fishing_data#adjusted Fishing data
@@ -21,23 +28,18 @@ ahackdat['ncell']<-ifelse(nchar(ahackdat$Cell)==1,
                        )
 
 ahackdat['sicell']<-paste(ahackdat$SiteCell,ahackdat$ncell, sep='')
+#date column registered as date by R
+ahackdat$Date<-as.Date(ahackdat$Date,format="%m/%d/%Y")
 #reorder to merge in correct order to match CCFRP data
 ahackdat<-ahackdat[,c('Date','Location','Protection','sicell','Drift','Species','Length','TagNum','Bloodsamp')]
-#merging identification columns in my data
-ghackdat<-do.call(paste,as.data.frame(ahackdat[,1:7]))
-#combine with unique data
-ghackdat<-cbind(ghackdat,ahackdat[8:9])
-#sort to line up with CCFRP data
-ghackdat$ghackdat<-sort(ghackdat$ghackdat,decreasing = F)
 
 
-#load CCFRP data
-library(readxl)
-Hack_Sample_Data <- read_excel("C:/Users/manic/Dropbox/Home computer/CalPoly/Rockfsh/Hack_Sample_Data.xlsx")
-View(Hack_Sample_Data)
+
+#####Formatting CCFRP data#####
 #change species codes to match my data
 ##will need to add more once I get all the data!
 fccfrp<-Hack_Sample_Data#fixed CCFRP data
+library(car)
 fccfrp$Species<-recode(fccfrp$Species,"
   'BLA'='RFBLK';
   'BLU'='RFBLU';
@@ -53,23 +55,52 @@ fccfrp$Area<-recode(fccfrp$Area,"
     'BL'='PBL';
     'PB'='PBN'
 ")
-#change month to number
-fccfrp$Month<-recode(fccfrp$Month,"
-    'July'='07';
-    'August'='08';
-    'September'='09'
-")
+#fix to date function
 #combine to get MM/DD/YYYY format
-fccfrp['Date']<-paste('0',paste(fccfrp$Month,fccfrp$Day,fccfrp$Year, sep='/'),sep='')
+fccfrp$Date<-paste(paste(fccfrp$Month,fccfrp$Day,fccfrp$Year, sep='/'),sep='')
+fccfrp$Date<-as.Date(fccfrp$Date,format="%B/%d/%Y")
+#change tagID to not be a charCter with a fucking letter 'O' at the end
+fccfrp$TagID<-as.numeric(substr(fccfrp$TagID,1,nchar(fccfrp$TagID)-1))
 #reorder columns to combime in proper order
-fccfrp<-fccfrp[,c('Date','Site','Area','Cell','Drift','Species','Length','TagID','StartLat','StartLon','EndLat','EndLon','Sex','Conditions')]
-#merge columns inorder to combine with my data
-gccfrp<-do.call(paste,as.data.frame(fccfrp[,1:7]))
+fccfrp<-fccfrp[,c('Date','Area','Site','Cell','Drift','Species','Length','TagID','StartLat','StartLon','EndLat','EndLon','Sex','Conditions')]
+
+
+#####Subsetting data that matches#####
+#subset those with tags
+tccfrp<-subset(fccfrp,!is.na(TagID))
+#subset those with tag#s
+thackdat<-subset(ahackdat,!is.na(TagNum))
+#merge matching tag numbers
+tfish<-merge(tccfrp,thackdat[c('TagNum','Bloodsamp')],by.x='TagID',by.y='TagNum',all.x=F,all.y=F)
+
+#subset matching length fish
+#remove tagged fish identified in tfish above
+notagccfrp<-fccfrp[is.na(fccfrp$TagID),]
+#merge columns in CCFRP data in order to combine with my data
+gccfrp<-as.data.frame(do.call(paste,as.data.frame((notagccfrp[,1:7]))))
+colnames(gccfrp)<-'fishID'
 #combine with unique data
-gccfrp<-cbind(gccfrp,fccfrp[8:14])
-#sort to line up with my data
-gccfrp$gccfrp<-sort(gccfrp$gccfrp,decreasing = F)
+gccfrp<-cbind(gccfrp,notagccfrp[8:14])
+#remove tagged fish identified in tfish above in my data also
+notaghackdat<-ahackdat[is.na(ahackdat$TagNum),]
+#merging identification columns in my data
+ghackdat<-do.call(paste,as.data.frame(notaghackdat[,1:7]))
+#combine with unique data
+ghackdat<-cbind(ghackdat,notaghackdat[9])
+#merge matching fish!
+mfish<-merge(gccfrp,ghackdat,by.x='fishID',by.y='ghackdat',all.x=F,all.y=F)
+
+#final subset that does not match
+#my data excluding tagged fish and matching fish (ghackdat already has tagged fish removed)
+whatfish<-ghackdat[!(ghackdat$ghackdat %in% mfish$fishID),]
+#count records with only unique fish
+onefish<-as.data.frame(table(whatfish$ghackdat))
+#find records that occur more than once
+morefish<-onefish[onefish$Freq > 1,]
 
 
-#Combine data sets!
-cfishdata<-merge(gccfrp,ghackdat,by='')#must be same length
+
+
+# how to deal with duplicates
+
+

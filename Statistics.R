47ed645f4e9812e9e6c444867feb0bb273890df8
niccihack <niccihack@gmail.com>
2017-03-03 21:41:53 -0800

    CF and SGR calculations

--- Statistics.R
+++ Statistics.R
@@ -12,32 +12,50 @@ RFOYTgrowth <- read_csv("~/RFOYT/RFOYT/RFOYTgrowth.csv")
 growth<-as.data.frame(RFOYTgrowth)
 
 
-#Calculate condition factor
+#calculate condition factor
+dat3<-data.frame()
+for (d in (1:length(b))){
+  m = dplyr::filter(b, mass.sl == 'mass')$value
+  l = dplyr::filter(b, mass.sl == 'SL')$value
+  cf = (m/(l^3))*100 #condition factor formula
+  dat3 = rbind(dat3, data.frame(b$PITtag, b$day, cf))#combine all into dataframe
+}
 
 
-#Calculate somatic growth rates
 #reorganize data into long form
 library(tidyr)
 b = tidyr::gather(growth, 'data.type','value', 5:16)
 b$mass.sl = gsub(".*\\-",'',b$data.type)#make separate data type column - mass or SL
 b$day = as.integer(substr( gsub("\\-.*","",b$data.type), 4,5))#make separate day column
+
 # generate all unique combinations of days 
 c = combn(unique(b$day), 2)
-dat = data.frame()#make empty dataframe for loop
 
+
+#Calculate somatic growth rates for mass
+dat = data.frame()#make empty dataframe for loop
 for (d in (1: ncol(c))){
   e = c[,d]
   #pull out the mass for those two days: 
   f = dplyr::filter(b, day == max(e) & mass.sl == 'mass')$value
   g = dplyr::filter(b, day == min(e) & mass.sl == 'mass')$value
-  sgr = log ( (f-g)/(max(e)-min(e))*100 )#somatic growth rate formula
-  treat = dplyr::filter(b, day == max(e) & mass.sl == 'mass')$treatment#pull out treatment
-  IGF1 = dplyr::filter(b, day == max(e) & mass.sl == 'mass')$IGF1#and IGF1
-  PITtag = dplyr::filter(b, day == max(e) & mass.sl == 'mass')$PITtag#and tag number
+  sgr.mass = log ( (f-g)/(max(e)-min(e))*100 )#somatic growth rate formula
   Comb = paste('Day',e[1], e[2], sep = '-')#make title
-  dat = rbind(dat, data.frame(PITtag, treat, IGF1, sgr, Comb))#combine all into dataframe
+  dat = rbind(dat, data.frame(b$PITtag, b$treatment, b$IGF1, sgr.mass, Comb))#combine all into dataframe
 }
 
+
+#calculate SGR for SL
+dat2 = data.frame()#make empty dataframe for loop
+for (d in (1: ncol(c))){
+  e = c[,d]
+  #pull out the sl for those two days: 
+  f = dplyr::filter(b, day == max(e) & mass.sl == 'SL')$value
+  g = dplyr::filter(b, day == min(e) & mass.sl == 'SL')$value
+  sgr.SL = log ( (f-g)/(max(e)-min(e))*100 )#somatic growth rate formula
+  Comb = paste('Day',e[1], e[2], sep = '-')#make title
+  dat2 = rbind(dat2, data.frame(b$PITtag, b$treatment, b$IGF1, sgr.SL, Comb))#combine all into dataframe
+}
 
  
 

c25fc44cd53beea2685f6a190f4aa6ffe30147a2
niccihack <niccihack@gmail.com>
2017-03-17 21:32:26 -0700

    Fixed SGR and CF loops

--- Statistics.R
+++ Statistics.R
@@ -18,44 +18,65 @@ b = tidyr::gather(growth, 'data.type','value', 5:16)
 b$mass.sl = gsub(".*\\-",'',b$data.type)#make separate data type column - mass or SL
 b$day = as.integer(substr( gsub("\\-.*","",b$data.type), 4,5))#make separate day column
 
-#calculate condition factor
+#calculate Condition factor
 dat.cf<-data.frame()
-for (d in (1:length(b))){
-  m = dplyr::filter(b, mass.sl == 'mass')$value
-  l = dplyr::filter(b, mass.sl == 'SL')$value
-  cf = (m/(l^3))*100 #condition factor formula
-  dat.cf = rbind(dat.cf, data.frame(b$PITtag, b$day, cf))#combine all into dataframe
-}
+m = dplyr::filter(b, mass.sl == 'mass')$value
+l = dplyr::filter(b, mass.sl == 'SL')$value
+cf = (m/(l^3))*100 #condition factor formula
+dat.cf = rbind(dat.cf, data.frame(b$PITtag, b$day, b$treatment, cf))#combine all into dataframe
+
+
 # generate all unique combinations of days 
-c = combn(unique(b$day), 2)
+comb = combn(unique(b$day), 2)
 
-
-#Calculate somatic growth rates for mass
-dat.mass = data.frame()#make empty dataframe for loop
-for (d in (1: ncol(c))){
-  e = c[,d]
-  #pull out the mass for those two days: 
-  f = dplyr::filter(b, day == max(e) & mass.sl == 'mass')$value
-  g = dplyr::filter(b, day == min(e) & mass.sl == 'mass')$value
-  sgr.mass = log ( (f-g)/(max(e)-min(e))*100 )#somatic growth rate formula
-  Comb = paste('Day',e[1], e[2], sep = '-')#make title
-  dat.mass = rbind(dat.mass, data.frame(b$PITtag, b$treatment, b$IGF1, sgr.mass, Comb))#combine all into dataframe
+#Calculate SGR for mass
+#subset out mass
+fish.mass <- dplyr::filter(b, mass.sl == 'mass')
+tag <- dplyr::filter(fish.mass, day == 0) %>% dplyr::select(.,PITtag)
+treatment <- dplyr::filter(fish.mass, day == 0) %>% dplyr::select(.,treatment)
+IGF1 <- dplyr::filter(fish.mass, day == 0) %>% dplyr::select(.,IGF1)
+dat.mass<-data.frame()
+for (d in 1:ncol(comb)){
+  fday = comb[2,d]
+  iday = comb[1,d]
+  f = dplyr::filter(fish.mass, day == fday)$value
+  i = dplyr::filter(fish.mass, day == iday)$value
+  sgr.mass = log (((f-i)/(fday - iday))*100 )#somatic growth rate formula
+  Comb = paste('Day',iday, fday, sep = '-')#make title
+  #rbind 
+  dat.mass = rbind(dat.mass, data.frame(tag, treatment, IGF1, Comb, sgr.mass))#combine all into dataframe  
 }
 
 
 #calculate SGR for SL
-dat.sl = data.frame()#make empty dataframe for loop
-for (d in (1: ncol(c))){
-  e = c[,d]
-  #pull out the sl for those two days: 
-  f = dplyr::filter(b, day == max(e) & mass.sl == 'SL')$value
-  g = dplyr::filter(b, day == min(e) & mass.sl == 'SL')$value
-  sgr.SL = log ( (f-g)/(max(e)-min(e))*100 )#somatic growth rate formula
-  Comb = paste('Day',e[1], e[2], sep = '-')#make title
-  dat.sl = rbind(dat.sl, data.frame(b$PITtag, b$treatment, b$IGF1, sgr.SL, Comb))#combine all into dataframe
+fish.sl <- dplyr::filter(b, mass.sl == 'SL')
+dat.sl<-data.frame()
+for (d in 1:ncol(comb)){
+  fday = comb[2,d]
+  iday = comb[1,d]
+  f = dplyr::filter(fish.sl, day == fday)$value
+  i = dplyr::filter(fish.sl, day == iday)$value
+  sgr.sl = log (((f-i)/(fday - iday))*100 )#somatic growth rate formula
+  Comb = paste('Day',iday, fday, sep = '-')#make title
+  #rbind 
+  dat.sl = rbind(dat.sl, data.frame(tag, treatment, IGF1, Comb, sgr.sl))#combine all into dataframe  
 }
 
- 
+
+
+#calculate condition factor
+dat.sl<-data.frame()
+for (d in 1:ncol(comb)){
+  fday = comb[2,d]
+  iday = comb[1,d]
+  f = dplyr::filter(fish.sl, day == fday)$value
+  i = dplyr::filter(fish.sl, day == iday)$value
+  sgr.sl = log (((f-i)/(fday - iday))*100 )#somatic growth rate formula
+  Comb = paste('Day',iday, fday, sep = '-')#make title
+  #rbind 
+  dat.sl = rbind(dat.sl, data.frame(tag, treatment, IGF1, Comb, sgr.sl))#combine all into dataframe  
+}
+
 
 #####Pairwise ANOVAs#####
 
@@ -70,21 +91,79 @@ myaov<-function(h){
 #apply to each day
 aov.stats <- sapply(unique(mass$day), myaov, simplify=FALSE)
 #check normality and residuals if necessary
-#plot(aov.mass[])
+plot(aov.mass[])
 #then run anova
 myanova<-function(i){
   anova.mass<-anova((lm(value~treatment,data=mass,day==i,na.action=na.omit)))
 }
 anova.stats<- sapply(unique(mass$day), myanova, simplify=FALSE)
 names(anova.stats)<-paste('Day -',unique(mass$day))#rename outputs to include days
-capture.output(anova.stats,file='anova.stats.csv')#export file
+capture.output(anova.stats,file='anova.stats.mass.csv')#export file
 
 #SL
+sl<-subset(b, mass.sl=='SL')#subset SL
+library(ggplot2)
+sl.plot=ggplot(sl, aes(treatment,value,colour=treatment))+ geom_boxplot()#look at data
+#check analysis of variance
+myaov<-function(h){
+  aov.sl<-aov(lm(value~treatment,data=sl,day==h,na.action=na.omit))
+}
+#apply to each day
+aov.stats <- sapply(unique(sl$day), myaov, simplify=FALSE)
+#check normality and residuals if necessary
+#plot(aov.sl[])
+#then run anova
+myanova<-function(i){
+  anova.sl<-anova((lm(value~treatment,data=sl,day==i,na.action=na.omit)))
+}
+anova.stats<- sapply(unique(sl$day), myanova, simplify=FALSE)
+names(anova.stats)<-paste('Day -',unique(sl$day))#rename outputs to include days
+capture.output(anova.stats,file='anova.stats.sl.csv')#export file
 
-
+#CF
+cf.plot=ggplot(dat.cf, aes(b.treatment,cf,colour=b.treatment))+ geom_boxplot()#look at data  
+#check analysis of variance
+myaov<-function(h){
+  aov.cf<-aov(lm(cf~b.treatment,data=dat.cf,b.day==h,na.action=na.omit))
+}
+#apply to each day
+aov.stats <- sapply(unique(dat.cf$b.day), myaov, simplify=FALSE)
+#check normality and residuals if necessary
+#plot(aov.sl[])
+#then run anova
+myanova<-function(i){
+  anova.cf<-anova((lm(cf~b.treatment,data=dat.cf,b.day==i,na.action=na.omit)))
+}
+anova.stats<- sapply(unique(dat.cf$b.day), myanova, simplify=FALSE)
+names(anova.stats)<-paste('Day -',unique(dat.cf$b.day))#rename outputs to include days
+capture.output(anova.stats,file='anova.stats.cf.csv')#export file
 
 #####Pearson's correlations#####
+#must remove Inf and NAs since pearson's cannot deal with them
+cor.sgr<-dplyr::filter(dat.sl, sgr.SL!=-Inf & !is.na(b.IGF1))
+
+
+for (k in unique(cor.sgr$Comb)){
+  tempx<-cor.sgr[cor.sgr$Comb==k,]
+  t
+}
+
+cor.sgr %>%
+  dplyr::select(b.IGF1,sgr.SL) %>%
+  dplyr::filter()
+
+cor(cor.sgr[c(3,4)])#need to do for each time interval
+cor.test(~ b.IGF1 + sgr.SL, data=cor.sgr[c(3,4)])
+
+cor.sgr %>% 
+  dplyr::group_by(Comb, b.treatment) %>% 
+  
+mycor<- function(j){
+  cor.test(~ b.IGF1 + sgr.SL, data=j)
+}
 
 
 
-####MANIVA#####
+
+
+####MANOVA#####
